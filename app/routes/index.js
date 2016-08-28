790b944fc754eb4cb1ce2ddc182edf55741387d1
catfriend <eakari@live.com>
2016-08-28 16:25:49 -0700

    added some styles and served up static filesdelte

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
   res.send(`
+  	<link rel="stylesheet" type="text/css" href="css/style.css">
       <h1>Natura Meetups</h1>
+      <img src="/images/misc/hero_2.jpg" alt="background" style="height: 300px, width: 1000px;">
       <p>Natura is a group of nature photographers who like to share their knowlege of the photographic arts.</p>
   `);
 });
